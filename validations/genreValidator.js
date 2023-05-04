4b63d1afb8f204d705b39371c735b27a3306d855
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-05-04 03:39:32 -0300

    genreValidator done

---
+++ validations/genreValidator.js
@@ -0,0 +1,18 @@
+const { check, body } = require('express-validator');
+
+module.exports = [
+    check('name')
+        .notEmpty().withMessage('El nombre es obligatorio').bail()
+        .isLength({ min: 2 }).withMessage('Mínimo de dos letras').bail()
+        .isAlpha('es-ES', {
+            ignore: " "
+        }).withMessage('Solo caracteres alfabéticos'),
+    check('active')
+        .notEmpty().withMessage('Elegir una opcion').bail(),    
+    /* check('favorite_movie_id')
+        .notEmpty().withMessage('Debes elegir una categoría'),
+    check('actors_id')
+        .notEmpty().withMessage('Debes elegir al menos un actor'), */   /*
+    .isNumeric({ no_symbols: true }).withMessage('Ingresar solo numeros').bail()
+    */
+]
