8105d20ff8b74849891fe757c327df9d9f3fdbe6
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-05-04 03:47:04 -0300

    TP finished genresController, routes y vistas done

--- src/controllers/genresController.js
+++ src/controllers/genresController.js
@@ -1,3 +1,4 @@
+const { validationResult } = require('express-validator');
 const db = require('../database/models');
 const sequelize = db.sequelize;
 
@@ -7,7 +8,7 @@ const genresController = {
         db.Genre.findAll()
             .then(genres => {
                 res.render('genresList.ejs', { genres })
-            })
+            }).catch(error => console.log(error));
     },
     'detail': (req, res) => {
         db.Genre.findByPk(req.params.id, {
@@ -16,7 +17,115 @@ const genresController = {
             .then(genre => {
                 //res.send(genre)
                 res.render('genresDetail.ejs', { genre });
-            });
+            }).catch(error => console.log(error));;
+    },
+    'add': (req, res) => {
+
+        return res.render('genresAdd')
+
+    },
+    'store': (req, res) => {
+        const errors = validationResult(req);
+        const { name } = req.body
+        //const { count } = db.Genre.findAndCountAll()
+       /* db.Genre.findAll().then( genres => {
+            const lastRanking = genres.length
+            return res.send(lastRanking /* count )
+        }
+        )
+        */
+         if (errors.isEmpty()) {
+            db.Genre.findAll().then( genres =>{      
+            db.Genre.create({
+                ...req.body,
+                name:name.trim(),
+                ranking: genres.length + 1
+            }).then(newGenre => {
+                console.log(newGenre);
+
+                return res.redirect('/genres')
+            })
+        }).catch(error => console.log(error));
+        } else {
+            return res.render('genresAdd',{
+                    errors: errors.mapped(),
+                    old: req.body,
+                }
+            )
+        }
+    },
+    'edit': function (req, res) {
+        db.Genre.findByPk(req.params.id)
+            .then(genre => {
+                // return res.send(actorToUpdate)
+                // res.send(actorToUpdate.release_date.toISOString())
+                //.split("T")[0] res.send([actorToUpdate,allMovies])
+                return res.render('genresEdit', {
+                    genre
+                })
+            })
+            .catch(error => console.log(error));
+    },
+    'update': function (req,res) {
+        const errors = validationResult(req);
+        const { name } = req.body
+        //const { count } = db.Genre.findAndCountAll()
+       /* db.Genre.findAll().then( genres => {
+            const lastRanking = genres.length
+            return res.send(lastRanking /* count )
+        }
+        )
+        */
+         if (errors.isEmpty()) {
+                  
+            db.Genre.update({
+                ...req.body,
+                name:name.trim(),                
+            },{
+                where:{
+                    id : req.params.id,
+                }
+            }).then(updatedGenre => {
+                console.log(updatedGenre);
+
+                return res.redirect('/genres')
+            }).catch(error => console.log(error));
+        } else {
+            db.Genre.findByPk(req.params.id)
+            .then(genre => {
+                // return res.send(actorToUpdate)
+                // res.send(actorToUpdate.release_date.toISOString())
+                //.split("T")[0] res.send([actorToUpdate,allMovies])
+                return res.render('genresEdit', {
+                    genre,
+                    errors: errors.mapped(),
+                    old: req.body,
+                })
+            })
+            .catch(error => console.log(error));
+        }
+    },
+    'delete': function (req, res) {
+        const { id } = req.params
+        db.Genre.findByPk(id)
+
+            
+            .then((genre) => {
+                // return res.send(Movie)
+                // .split("T")[0] res.send([movie,allGenres])
+                return res.render('genresDelete', { genre } )
+            })
+            .catch(error => console.log(error));
+    },
+    'destroy': function (req, res) {
+        const { id } = req.params
+        db.Genre.destroy({
+            where: { id: id }
+        })
+            .then(() => {
+                return res.redirect('/genres')
+            })
+            .catch(error => console.log(error));
     }
 
 }
--- src/routes/genresRoutes.js
+++ src/routes/genresRoutes.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const genresController = require('../controllers/genresController');
+const genreValidator = require('../../validations/genreValidator');
 
 router.get('/genres', genresController.list);
 router.get('/genres/detail/:id', genresController.detail);
+router.get('/genres/add', genresController.add)
+router.post('/genres/create', genreValidator, genresController.store)
+router.get('/genres/edit/:id', genresController.edit)
+router.post('/genres/update/:id', genreValidator, genresController.update)
+router.get('/genres/delete/:id', genresController.delete);
+router.post('/genres/delete/:id', genresController.destroy);
+
 
 
 module.exports = router;
